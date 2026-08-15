95816293814acfd7c38ca6c7d13baf26cb17ceaa
tahasinatulljannat890-design <tahasinatulljannat890@gmail.com>
2026-08-15 11:34:29 +0600

    Implement sum and product calculation for 5 numbers
    This program takes 5 integers as input, calculates their sum and product, and prints the results.

---
+++ Array/task9_sum_product.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(){
+    int num[5], sum = 0, product = 1;
+    printf("Enter 5 numbers = ");
+
+    //input values of num array
+    for(int i=0; i<5; i++){
+        scanf("%d", &num[i]);
+    }
+
+    //Calculate sum of array elements
+    for(int i=0; i<5; i++){
+        sum+=num[i];
+    }
+    printf("Sum = %d",sum);
+
+    //Calculate product of array elements
+    for(int i=0; i<5; i++){
+        product*=num[i];
+    }
+    printf("Product = %d", product);
+
+    return 0; 
+}
